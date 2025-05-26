975953edee189e99911a01a443001587b78ee45b
KerrMatt <matthew.kerr@bio.au.dk>
2025-05-26 14:55:01 +0200

    Code from the end of 26th May 2025; clean data and run basic EFA to plot.

--- Code/Eastern Pacific Bivalves.R
+++ Code/Eastern Pacific Bivalves.R
@@ -3,9 +3,11 @@
 # Library Load ----
 ## Helpers
 library(tidyverse)
+library(scico)
 
-## Data access
+## Data access/cleaner
 library(robis)
+library(CoordinateCleaner)
 
 ## Analysis packages
 library(psych)
@@ -33,15 +35,65 @@ decimal_places <- function(x) {
 # Case Study 1: Eastern Pacific Bivalves ----
 
 ## Get the data from robis and clean
-eastPac_bivalves <- robis::occurrence(scientificname = "Bivalvia", areaid = "31908,34312,265") %>%
+eastPac_bivalves_raw <- robis::occurrence(scientificname = "Bivalvia", areaid = "31908,34312,265") %>%
   filter(., !is.na(species)) %>%
-  filter(., date_year >= 1900 | as.numeric(year) >= 1900)
+  filter(., date_year >= 1900 | as.numeric(year) >= 1900) %>%
+  filter(., decimalLongitude < 0)
 
 ## Location accuracy
-eastPac_bivalves <- eastPac_bivalves[which(unlist(lapply(eastPac_bivalves$decimalLatitude, decimal_places)) >= 2),]
+eastPac_bivalves_raw <- eastPac_bivalves_raw[which(unlist(lapply(eastPac_bivalves_raw$decimalLatitude, decimal_places)) >= 2),]
 
 ## Bin into latitudinal bands
-bivalves_bins <- floor(eastPac_bivalves$decimalLatitude)
+bivalves_bins <- floor(eastPac_bivalves_raw$decimalLatitude)
+eastPac_bivalves_raw$LatitudeBin <- bivalves_bins
 
 ## Only keep easternmost part of each band
+eastPac_bivalves <- eastPac_bivalves_raw[0,]
+for(i in unique(bivalves_bins)){
+  coast <- max(eastPac_bivalves_raw$decimalLongitude[which(bivalves_bins == i)])
+  coast_limit <- coast - 0.5
+  
+  eastPac_bivalves <- rbind(eastPac_bivalves, eastPac_bivalves_raw[which(eastPac_bivalves_raw$decimalLongitude >= coast_limit & bivalves_bins == i),])
+}
 
+## Build presence absence table
+pa_bivalve <- table(eastPac_bivalves$species, eastPac_bivalves$LatitudeBin)
+pa_bivalve[pa_bivalve > 1] <- 1
+
+## Calculate objective number of factors
+
+psych::fa.parallel(x = pa_bivalve)
+
+
+## RUn factor analyses:
+# Using number from parallel
+fa_bivalve_objective <- psych::fa(r = pa_bivalve, nfactors = 11)
+fa_bivalve_objective_loadings <- as.data.frame(fa_bivalve_objective$loadings[,c(1:11)]) %>%
+  rownames_to_column(var = "LatitudinalBin") %>%
+  pivot_longer(cols = -"LatitudinalBin",
+               names_to = "Factor",
+               values_to = "value")
+
+fa_bivalve_objective_loadings$Factor <- as.factor(fa_bivalve_objective_loadings$Factor)
+fa_bivalve_objective_loadings$LatitudinalBin <- as.numeric(fa_bivalve_objective_loadings$LatitudinalBin)
+
+# Using number from Valentine
+fa_bivalve_valentine <- psych::fa(r = pa_bivalve, nfactors = 6)
+fa_bivalve_valentine_loadings <- as.data.frame(fa_bivalve_valentine$loadings[,c(1:6)]) %>%
+  rownames_to_column(var = "LatitudinalBin") %>%
+  pivot_longer(cols = -"LatitudinalBin",
+               names_to = "Factor",
+               values_to = "value")
+
+fa_bivalve_valentine_loadings$Factor <- as.factor(fa_bivalve_valentine_loadings$Factor)
+fa_bivalve_valentine_loadings$LatitudinalBin <- as.numeric(fa_bivalve_valentine_loadings$LatitudinalBin)
+
+## Plot:
+ggplot(data = fa_bivalve_objective_loadings, aes(y = value, x = LatitudinalBin, group = Factor, colour = Factor, fill = Factor)) + 
+  geom_smooth(fill = NA) + theme_bw() + xlim(0,66) + coord_flip() + scale_color_scico_d(palette = "batlowK") + 
+  scale_fill_scico_d(palette = "batlowK") + geom_vline(xintercept = c(24, 36, 56, 63, 28))
+
+ggplot(data = fa_bivalve_valentine_loadings, aes(y = value, x = LatitudinalBin, group = Factor, colour = Factor, fill = Factor)) + 
+  geom_smooth(fill = NA, method = "loess") + theme_bw() + xlim(0,66) + coord_flip() + scale_color_scico_d(palette = "batlowK") + 
+  scale_fill_scico_d(palette = "batlowK") + geom_vline(xintercept = c(24, 36, 56, 63, 28))
+  
